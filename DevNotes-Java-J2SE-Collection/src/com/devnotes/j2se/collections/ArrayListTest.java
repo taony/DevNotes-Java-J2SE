c761be776287a81182a0d2de2e82481a3266856e
taony <taonyzhang@gmail.com>
2017-02-21 23:40:13 +0800

    arraylist

---
+++ DevNotes-Java-J2SE-Collection/src/com/devnotes/j2se/collections/ArrayListTest.java
@@ -0,0 +1,14 @@
+package com.devnotes.j2se.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<String> list =new ArrayList<String>();
+	}
+
+}
