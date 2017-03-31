dc15284802918871f3142fba0590e0149b26ecee
taony <taonyzhang@gmail.com>
2017-03-31 23:22:03 +0800

    file

--- DevNotes-Java-J2SE-IO/src/com/devnotes/j2se/io/FileTest.java
+++ DevNotes-Java-J2SE-IO/src/com/devnotes/j2se/io/FileTest.java
@@ -1,10 +1,15 @@
 package com.devnotes.j2se.io;
 
+import java.io.File;
+
 public class FileTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("File.separator："+File.separator);
+
+		System.out.println("File.pathSeparator："+File.pathSeparator);
 	}
 
 }
