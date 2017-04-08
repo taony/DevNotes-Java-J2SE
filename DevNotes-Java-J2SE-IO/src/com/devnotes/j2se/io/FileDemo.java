f5a66aabd9aaeda2eeb0b6597ce343a8850549ef
taony <taonyzhang@gmail.com>
2017-04-08 10:39:58 +0800

    file

--- DevNotes-Java-J2SE-IO/src/com/devnotes/j2se/io/FileDemo.java
+++ DevNotes-Java-J2SE-IO/src/com/devnotes/j2se/io/FileDemo.java
@@ -1,15 +1,41 @@
 package com.devnotes.j2se.io;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-		System.out.println("File.separator："+File.separator);
+        //1、文件路径分隔符，windows操作系统是："\"，Linux操作系统是："/"
+        System.out.println("File.separator：" + File.separator);
 
-		System.out.println("File.pathSeparator："+File.pathSeparator);
-	}
+        //2、文件路径分隔符，windows操作系统是：";"，Linux操作系统是：":"
+        System.out.println("File.pathSeparator：" + File.pathSeparator);
 
+        try {
+
+            //3、创建文件对象，构造函数是pathname，完整的文件路径。
+            File file = new File("text.txt");
+
+            //4、判断文件是否存在
+            if (file.exists()) {
+
+                //5、删除文件
+                file.delete();
+            }
+
+            //6、创建文件
+            file.createNewFile();
+
+            file.isDirectory();
+
+            //7、文件长度
+            System.out.println(file.length());
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
 }
---
+++ DevNotes-Java-J2SE-IO/src/com/devnotes/j2se/io/RandomAccessFileDemo.java
@@ -0,0 +1,8 @@
+package com.devnotes.j2se.io;
+
+/**
+ * Created by taony on 2017/4/8.
+ */
+public class RandomAccessFileDemo {
+
+}
