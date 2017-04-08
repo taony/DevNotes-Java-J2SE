2514f557be8f76f0b85e6664dc4907e8825170e1
taony <taonyzhang@gmail.com>
2017-04-08 23:38:57 +0800

    file

--- DevNotes-Java-J2SE-IO/src/com/devnotes/j2se/io/FileDemo.java
+++ DevNotes-Java-J2SE-IO/src/com/devnotes/j2se/io/FileDemo.java
@@ -13,14 +13,29 @@ public class FileDemo {
         //2、文件路径分隔符，windows操作系统是：";"，Linux操作系统是：":"
         System.out.println("File.pathSeparator：" + File.pathSeparator);
 
+        String pathRoot = FileDemo.class.getProtectionDomain().getCodeSource().getLocation().toString();
+        System.out.println(pathRoot);//获取程序的根目录
+
         try {
 
+            String strDir = pathRoot + "/FILES/01/";
+
+
+            File dir = new File(strDir);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+
+            if (dir.isDirectory()) {
+                System.out.println("文件目录：" + dir);
+            }
+
             //3、创建文件对象，构造函数是pathname，完整的文件路径。
-            File file = new File("text.txt");
+            File file = new File(strDir + "file.txt");
 
             //4、判断文件是否存在
             if (file.exists()) {
-
                 //5、删除文件
                 file.delete();
             }
@@ -28,10 +43,9 @@ public class FileDemo {
             //6、创建文件
             file.createNewFile();
 
-            file.isDirectory();
 
             //7、文件长度
-            System.out.println(file.length());
+            System.out.println("文件内容长度：" + file.length());
 
         } catch (IOException ex) {
             ex.printStackTrace();
--- DevNotes-Java-J2SE-IO/src/com/devnotes/j2se/io/RandomAccessFileDemo.java
+++ DevNotes-Java-J2SE-IO/src/com/devnotes/j2se/io/RandomAccessFileDemo.java
@@ -5,4 +5,7 @@ package com.devnotes.j2se.io;
  */
 public class RandomAccessFileDemo {
 
+    static void main(){
+
+    }
 }
