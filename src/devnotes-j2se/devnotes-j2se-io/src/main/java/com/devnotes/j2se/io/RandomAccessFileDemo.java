aa36fadd60a62c7a8a4132fd372883a01d654d16
taony <taony@users.noreply.github.com>
2018-06-22 01:43:55 +0800

    raf

--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/RandomAccessFileDemo.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/RandomAccessFileDemo.java
@@ -1,6 +1,24 @@
 package com.devnotes.j2se.io;
 
+import java.io.File;
+import java.io.RandomAccessFile;
+
 public class RandomAccessFileDemo {
 
-    
+    public static void main(String[] args) {
+
+    }
+
+    public void testRaf(){
+        File file=new File(".");
+
+        try {
+            RandomAccessFile raf1=new RandomAccessFile(file, "r");
+
+            raf1.read();
+
+        }catch (Exception ex){
+
+        }
+    }
 }
