779127e5663f164850160c8f6170c18b1903b05d
taony <taony@users.noreply.github.com>
2018-07-04 00:20:26 +0800

    bis

--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/BufferedInputStreamDemo.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/BufferedInputStreamDemo.java
@@ -1,5 +1,8 @@
 package com.devnotes.j2se.io;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
 /**
  * @author: taony
  * @email:taonyzhang@gmail.com
@@ -11,6 +14,21 @@ public class BufferedInputStreamDemo {
 
     public static void main(String[] args) {
 
-
+        try {
+            FileInputStream fis=new FileInputStream("BISDemo.txt");
+            BufferedInputStream bis=new BufferedInputStream(fis);
+            String content=null;
+            //自己定义一个缓冲区
+            byte[] buffer=new byte[10240];
+            int flag=0;
+            while((flag=bis.read(buffer))!=-1){
+                content+=new String(buffer, 0, flag);
+            }
+            System.out.println(content);
+            //关闭的时候只需要关闭最外层的流就行了
+            bis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
