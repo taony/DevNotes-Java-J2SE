4c9c01df9e7f94aa20a699f4642c19168feb9e97
taony <taonyzhang@gmail.com>
2018-07-11 23:17:46 +0800

    bos

--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/BufferedInputStreamDemo.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/BufferedInputStreamDemo.java
@@ -1,7 +1,6 @@
 package com.devnotes.j2se.io;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import java.io.*;
 
 /**
  * @author: taony
@@ -12,26 +11,51 @@ import java.io.FileInputStream;
 public class BufferedInputStreamDemo {
 
     public static void main(String[] args) {
+        //readFile();
+
+        copyFile();
+    }
+
+    private static void readFile() {
         try {
-            FileInputStream fis=new FileInputStream("BISDemo.txt");
-            BufferedInputStream bis=new BufferedInputStream(fis);
-            String content=null;
-
+            File file = new File("C:\\txt.log");
+            if (null != file) {
+                System.out.println(file.length());
+            }
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            String content = null;
             //自己定义一个缓冲区
-            byte[] buffer=new byte[10240];
-            int flag=0;
+            byte[] buffer = new byte[1024];
+            int flag = 0;
 
-            while((flag=bis.read(buffer))!=-1){
-                content+=new String(buffer, 0, flag);
+            while ((flag = bis.read(buffer)) != -1) {
+                System.out.println("flag:" + flag);
+                content += new String(buffer, 0, flag);
             }
 
             System.out.println(content);
-
             //关闭的时候只需要关闭最外层的流就行了
             bis.close();
-
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void copyFile() {
+        String file1 = "C:\\txt.log";
+        String file2 = "C:\\txt1.log";
+
+        try {
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file1));
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file2));
+            byte[] b = new byte[128];
+            while (bis.read(b) != -1) {
+                bos.write(b);
+            }
+            bos.flush();
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 }
