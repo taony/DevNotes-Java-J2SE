2ad5f0c737a4716fd83e027cc1d6348437600a11
taony <taony@users.noreply.github.com>
2018-07-31 23:06:25 +0800

    reader

--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/BufferedReaderDemo.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/BufferedReaderDemo.java
@@ -1,5 +1,7 @@
 package com.devnotes.j2se.io;
 
+import java.io.*;
+
 /**
  * @author: taony
  * @email:taonyzhang@gmail.com
@@ -9,6 +11,24 @@ package com.devnotes.j2se.io;
 public class BufferedReaderDemo {
 
     public static void main(String[] args) {
+
+
+        try {
+
+            Reader reader=new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Temp\\reader.txt"),"GBK"));
+
+            char[] chars=new char[128];
+
+            while (reader.read(chars)>0){
+                String str=new String(chars);
+                System.out.println(str);
+            }
+
+            reader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
