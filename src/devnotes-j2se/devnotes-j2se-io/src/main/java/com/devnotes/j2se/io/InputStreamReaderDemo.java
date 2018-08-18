354bfdc94de5dbc05af324cb53e70357e6871af3
devnotes <taonyzhang@gmail.com>
2018-08-18 22:33:17 +0800

    thread

--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/InputStreamReaderDemo.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/InputStreamReaderDemo.java
@@ -6,24 +6,19 @@ import java.io.InputStreamReader;
 public class InputStreamReaderDemo {
 
     public static void main(String[] args) {
-
-        try (
-                //将 System.in 对象转换为 Reader 对象
-                InputStreamReader reader = new InputStreamReader(System.in);
-                BufferedReader br = new BufferedReader(reader)) {
-
+        try {
+            //将 System.in 对象转换为 Reader 对象
+            InputStreamReader reader = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(reader);
             String line = null;
-
+            System.out.print("请输入内容：");
             while ((line = br.readLine()) != null) {
                 if (line.equals("exit")) {
                     System.exit(1);
                 }
                 System.out.println("输入内容为：" + line);
             }
-
         } catch (Exception ex) {
-            ex.printStackTrace();
         }
     }
-
 }
