691cbc1e20ed32fbd314223ccf20d010e30aefdc
devnotes <taonyzhang@gmail.com>
2018-08-29 23:29:04 +0800

    java-io
    Signed-off-by: devnotes <taonyzhang@gmail.com>

--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/ObjectOutputStreamDemo.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/ObjectOutputStreamDemo.java
@@ -35,16 +35,11 @@ public class ObjectOutputStreamDemo {
 }
 
 class User implements Serializable {
-
     private           String  userName;
     private           String  userPwd;
     private transient Integer age;
 
-
-    public User() {
-
-    }
-
+    //序列化对象不需要提供默认构造器，序列化时不需要依赖于默认构造器
     public User(String name, String pwd, int age) {
         this.userName = name;
         this.userPwd = pwd;
