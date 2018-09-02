5375316c7b14283f5c48027740cc8db152faea0d
devnotes <taonyzhang@gmail.com>
2018-09-02 10:10:48 +0800

    java-io
    Signed-off-by: devnotes <taonyzhang@gmail.com>

--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/FilterReaderDemo.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/FilterReaderDemo.java
@@ -2,17 +2,18 @@ package com.devnotes.j2se.io;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.FilterReader;
-import java.io.Reader;
 
+/**
+ * @author Administrator
+ */
 public class FilterReaderDemo {
 
     public static void main(String[] args) {
 
         try {
-            File file = new File("C:\\temp\\FilterReaderDemo.txt");
-            Reader reader = new FileReader(file);
-            char[] arr = new char[1024];
+            var file   = new File("C:\\temp\\FilterReaderDemo.txt");
+            var reader = new FileReader(file);
+            var arr    = new char[1024];
             while (reader.read(arr) > 0) {
                 System.out.println(new String(arr));
             }
--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/common/FileTestUtil.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/common/FileTestUtil.java
@@ -2,6 +2,9 @@ package com.devnotes.j2se.io.common;
 
 import java.io.File;
 
+/**
+ * @author Administrator
+ */
 public class FileTestUtil {
     public static String getTempDir() {
         return System.getProperty("user.dir") + File.separatorChar + "devnotes-j2se-io\\src\\main\\resources\\temp" + File.separatorChar;
