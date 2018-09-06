fd1e4690a8921c84be07582487537a03e8544b91
devnotes <taonyzhang@gmail.com>
2018-09-06 08:15:03 +0800

    java-io
    Signed-off-by: devnotes <taonyzhang@gmail.com>

--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/RandomAccessFileDemo.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/RandomAccessFileDemo.java
@@ -1,30 +1,32 @@
 package com.devnotes.j2se.io;
 
+import com.devnotes.j2se.io.common.FileTestUtil;
+
 import java.io.File;
 import java.io.RandomAccessFile;
 import java.util.UUID;
 
+/**
+ * @author Administrator
+ */
 public class RandomAccessFileDemo {
 
     public static void main(String[] args) {
-        testRaf();
+        //输出数据
+        writeData();
     }
 
-
-    public static void testRaf() {
-
-        File file = new File("/resources/temp/RandomAccessFileDemo.txt");
-
+    public static void writeData() {
+        File file = new File(FileTestUtil.getTempDir() + "RandomAccessFileDemo_Write.txt");
         try {
-
             if (!file.exists()) {
                 file.createNewFile();
             }
-
             RandomAccessFile raf = new RandomAccessFile(file, "rw");
-
             raf.writeChars(UUID.randomUUID().toString());
-
+            raf.writeBoolean(new Boolean("1"));
+            raf.write(1);
+            raf.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
