09202dc52a94ff7e0eb66df83d67c4f8cbf73905
devnotes <taonyzhang@gmail.com>
2018-09-06 23:20:07 +0800

    11

--- src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/RandomAccessFileDemo.java
+++ src/devnotes-j2se/devnotes-j2se-io/src/main/java/com/devnotes/j2se/io/RandomAccessFileDemo.java
@@ -13,19 +13,31 @@ public class RandomAccessFileDemo {
 
     public static void main(String[] args) {
         //输出数据
-        writeData();
+        write();
     }
 
-    public static void writeData() {
+    public static void write() {
         File file = new File(FileTestUtil.getTempDir() + "RandomAccessFileDemo_Write.txt");
         try {
             if (!file.exists()) {
                 file.createNewFile();
             }
-            RandomAccessFile raf = new RandomAccessFile(file, "rw");
+            RandomAccessFile raf = new RandomAccessFile(file, "w");
             raf.writeChars(UUID.randomUUID().toString());
             raf.writeBoolean(new Boolean("1"));
             raf.write(1);
+            raf.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void read(){
+        File file = new File(FileTestUtil.getTempDir() + "RandomAccessFileDemo.txt");
+        try {
+            RandomAccessFile raf = new RandomAccessFile(file, "r");
+            Byte[] bytes=new Byte[1024];
+            raf.readFully(bytes);
             raf.close();
         } catch (Exception ex) {
             ex.printStackTrace();
