65cb47963e49141ef7040b59ed72157a19ae21fe
devnotes <taonyzhang@gmail.com>
2018-09-21 23:57:10 +0800

    java nio
    Signed-off-by: devnotes <taonyzhang@gmail.com>

--- src/devnotes-j2se/devnotes-j2se-nio/src/main/java/com/devnotes/j2se/nio/ByteBufferDemo.java
+++ src/devnotes-j2se/devnotes-j2se-nio/src/main/java/com/devnotes/j2se/nio/ByteBufferDemo.java
@@ -1,5 +1,14 @@
 package com.devnotes.j2se.nio;
 
+import java.io.RandomAccessFile;
+
 public class ByteBufferDemo {
 
+    public static void main(String[] args) throws Exception{
+
+        RandomAccessFile raf=new RandomAccessFile(FileTestUtil.getTempDir()+"ByteBufferDemo.txt","rw");
+
+
+
+    }
 }
