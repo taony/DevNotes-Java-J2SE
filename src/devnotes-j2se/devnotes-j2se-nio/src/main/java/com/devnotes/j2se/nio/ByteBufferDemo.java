ef03d04ba76570951ed968ae0e5f9b0a49cdf17b
devnotes <taonyzhang@gmail.com>
2018-09-21 23:59:33 +0800

    11

--- src/devnotes-j2se/devnotes-j2se-nio/src/main/java/com/devnotes/j2se/nio/ByteBufferDemo.java
+++ src/devnotes-j2se/devnotes-j2se-nio/src/main/java/com/devnotes/j2se/nio/ByteBufferDemo.java
@@ -1,12 +1,15 @@
 package com.devnotes.j2se.nio;
 
 import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
 
 public class ByteBufferDemo {
 
     public static void main(String[] args) throws Exception{
 
         RandomAccessFile raf=new RandomAccessFile(FileTestUtil.getTempDir()+"ByteBufferDemo.txt","rw");
+
+        FileChannel channel=raf.getChannel();
 
 
 
