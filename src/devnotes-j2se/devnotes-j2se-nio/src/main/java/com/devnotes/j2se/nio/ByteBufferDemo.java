aef11fc23858ed1b216b50a049aa5034fa82f385
devnotes <taonyzhang@gmail.com>
2018-11-14 00:02:38 +0800

    java collection
    Signed-off-by: devnotes <taonyzhang@gmail.com>

--- src/devnotes-j2se/devnotes-j2se-nio/src/main/java/com/devnotes/j2se/nio/ByteBufferDemo.java
+++ src/devnotes-j2se/devnotes-j2se-nio/src/main/java/com/devnotes/j2se/nio/ByteBufferDemo.java
@@ -12,18 +12,20 @@ import java.nio.charset.CharsetDecoder;
  */
 public class ByteBufferDemo {
 
-    public static void main(String[] args) throws Exception{
+    private static final int SIZE = 48;
+
+    public static void main(String[] args) throws Exception {
 
         //打开读写模式
-        RandomAccessFile raf=new RandomAccessFile(FileTestUtil.getTempDir()+"ByteBufferDemo.txt","rw");
+        RandomAccessFile raf = new RandomAccessFile(FileTestUtil.getTempDir() + "ByteBufferDemo.txt", "rw");
 
         //获取通道
-        FileChannel channel=raf.getChannel();
+        FileChannel channel = raf.getChannel();
 
         //定义缓冲区
-        ByteBuffer byteBuffer=ByteBuffer.allocate(48);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(SIZE);
 
-        while (channel.read(byteBuffer)>0){
+        while (channel.read(byteBuffer) > 0) {
 
             byteBuffer.flip();
 
@@ -40,10 +42,10 @@ public class ByteBufferDemo {
      * @return
      * @throws Exception
      */
-    public static String getString(ByteBuffer byteBuffer) throws Exception{
-        Charset charset=Charset.forName("UTF-8");
-        CharsetDecoder decoder=charset.newDecoder();
-        CharBuffer charBuffer=decoder.decode( byteBuffer.asReadOnlyBuffer());
+    public static String getString(ByteBuffer byteBuffer) throws Exception {
+        Charset        charset    = Charset.forName("UTF-8");
+        CharsetDecoder decoder    = charset.newDecoder();
+        CharBuffer     charBuffer = decoder.decode(byteBuffer.asReadOnlyBuffer());
         return charBuffer.toString();
     }
 }
